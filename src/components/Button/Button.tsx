f367823a25cdf31c8eaac42704143dedeb48d37f
Pascal <pascal@pascal.com>
2023-06-14 18:20:10 -0400

    Initial commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+interface ButtonProps {
+  label: string;
+}
+
+export default function Button({ label }: ButtonProps) {
+  return (
+    <button>{label}</button>
+  );
+}
